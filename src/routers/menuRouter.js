3d3e3b305e1e1dab9d92cdacc5ed01f649b97137
박성환 <aixion@Aixion-MacBookPro.local>
2023-08-17 17:18:00 +0900

    feat/router

--- src/routers/menuRouter.js
+++ src/routers/menuRouter.js
@@ -1,11 +1,14 @@
 import is from '@sindresorhus/is';
 import { Router } from 'express';
+import { coffeeMenuService } from '../services/CoffeMenuservice';
 
 const menuRouter = Router();
 
 menuRouter.get('/menus', async (req, res, next) => {
   try {
     // coffeeMenuService의 getMenus 메서드를 사용해 응답해주세요
+    const menus = coffeeMenuService.getMenus();
+    res.send();
   } catch (error) {
     next(error);
   }
@@ -19,7 +22,14 @@ menuRouter.post('/menus/create', async (req, res, next) => {
       );
     }
     // coffeeMenuService의 addCoffeeMenu 메서드를 사용해 응답해주세요
+    const menu = coffeeMenuService.addCoffeeMenu({
+      name: req.body.name,
+      price: req.body.price,
+      ice: req.body.ice,
+      hot: req.body.hot,
+    });
     // status code는 201로 해주세요
+    tes.status(201).send(menu);
   } catch (error) {
     next(error);
   }
--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -5,8 +5,10 @@ import { orderService } from '../services/OrderService';
 const orderRouter = Router();
 
 orderRouter.get('/orders', function (req, res, next) {
-  try {ㅈ
+  try {
     // orderService의 getAllOrders 메서드를 사용해 응답해주세요
+    const orders = orderService.getAllOrders();
+    res.send(orders);
   } catch (error) {
     next(error);
   }
@@ -16,6 +18,8 @@ orderRouter.get('/orders/:id', function (req, res, next) {
   try {
     // orderService의 getCoffee 메서드를 사용해 coffee 객체를 구하고
     // `주문하신 커피는 ${커피이름}입니다. 잠시만 기다려주세요`를 응답으로 보내주세요
+    const orders = orderService.getCoffee(req.params.id);
+    res.send(`주문하신 커피는 ${커피이름}입니다. 잠시만 기다려주세요`);
   } catch (error) {
     next(error);
   }
@@ -30,7 +34,9 @@ orderRouter.post('/orders/create', function (req, res, next) {
     }
 
     // orderService의 orderCoffee를 이용해 order를 응답으로 보내주세요
+    const order = orderService.orderCoffee(req.body.name, req.body.ice);
     // status 코드는 201로 보내주세요
+    res.status(201).send(order);
   } catch (error) {
     next(error);
   }
